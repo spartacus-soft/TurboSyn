525187d62cf9ca3cc39aecad176b981393a42b6d
spartacus-soft <149551882+spartacus-soft@users.noreply.github.com>
2024-10-24 13:45:15 +0800

    add libTurboSyn

---
+++ TurboSyn/TurboSyn.h
@@ -0,0 +1,74 @@
+#include <stdint.h>
+
+// 扫描状态
+typedef enum {
+	SUCCESS = 0,
+	CANCELLED = 1,
+	COMPLETED = 2,
+} TurboSynScanState;
+
+// 扫描成功或完成的结果
+typedef struct {
+	// 扫描状态
+	TurboSynScanState State;
+	// TCP端口
+	int32_t Port;
+	// 当前IP内容长度
+	int32_t IPLength;
+	// 当前IP的内容
+	uint8_t IPAddress[16];
+} TurboSynScanResult;
+
+// 扫描进度
+typedef struct {
+	// 当前IP数量
+	uint64_t CurrentCount;
+	// 要扫描的IP总数量
+	uint64_t TotalCount;
+	// 当前扫描的IP内容长度
+	int32_t IPLength;
+	// 当前扫描的I的内容
+	uint8_t IPAddress[16];
+} TurboSynScanProgress;
+
+// 扫描器
+typedef void* TurboSynScanner;
+
+// 扫描成功回调
+typedef void (*TurboSuccessCallback)(
+	// 扫描结果
+	TurboSynScanResult scanResult,
+	// 用户自定义参数
+	void* userParam);
+
+// 扫描进度回调
+typedef void (*TurboProgressCallback)(
+	// 扫描进度
+	TurboSynScanProgress scanProgress,
+	// 用户自定义参数
+	void* userParam);
+
+// 创建扫描器
+// 失败则返回NULL
+extern "C" TurboSynScanner TurboSynCreateScanner(
+	// CIDR或IP地址文本内容，一行一条记录
+	const char* content);
+
+// 开始扫描
+extern "C" bool TurboSynStartScan(
+	// 扫描器
+	TurboSynScanner scanner,
+	// TCP端口
+	int32_t port,
+	// 成功回调
+	TurboSuccessCallback successCallback,
+	// 进度回调
+	TurboProgressCallback progressCallback,
+	// 用户自定义参数
+	void* userParam);
+
+// 取消扫描器的所有扫描任务
+extern "C" bool TurboSynCancelScan(TurboSynScanner scanner);
+
+// 释放扫描器
+extern "C" void TurboSynFreeScanner(TurboSynScanner scanner);
